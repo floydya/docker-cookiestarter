04b339c27968deb33e2e1fed4dd256b5263bc0a6
Vlad Galatskiy <xfloydya@gmail.com>
2019-02-14 10:55:59 +0200

    celery-beat

--- {{cookiecutter.project}}/server/app/settings/default.py
+++ {{cookiecutter.project}}/server/app/settings/default.py
@@ -137,10 +137,6 @@ EMAIL_CONFIG = env.email_url('DJANGO_EMAIL_URL', 'consolemail://127.0.0.1')
 vars().update(EMAIL_CONFIG)
 
 CELERY_BROKER_URL = env.str('CELERY_BROKER_URL')
-# CELERY_RESULT_BACKEND = env.str('CELERY_BROKER_URL') + "/0"
-# CELERY_ACCEPT_CONTENT = ['json']
-# CELERY_TASK_SERIALIZER = 'json'
-# CELERY_RESULT_SERIALIZER = 'json'
 
 if not DEBUG:
     LOGGING = {
